a355f92c6aca6d81ca817dd610143d2978ce5260
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-08-20 01:33:44 -0400

    Grid is now displayed

--- src/Components/BoardContainer.js
+++ src/Components/BoardContainer.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { useState } from "react";
+
+const numRows = 50;
+const numCols = 50;
 
 const BoardContainer = () => {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  });
+  console.log(grid);
   return (
-    <div className="boardContainer">
-     
+    <div
+      className="boardContainer"
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${numCols}, auto)`,
+      }}
+    >
+      {grid.map((rows, i) =>
+        rows.map((col, k) => (
+          <div
+            key={i - k}
+            style={{
+              width: 10,
+              height: 10,
+              backgroundColor: grid[i][k] ? "#F68E5F" : undefined,
+              border: "1px solid #595959",
+            }}
+          />
+        ))
+      )}
     </div>
   );
 };
